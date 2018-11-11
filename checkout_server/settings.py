59508644c08c0cdbcc187b1ccd90856427c424e8
Bogdan Ciobotaru <bogdan.ciobotaru1@gmail.com>
2018-11-11 20:36:46 +0100

    add new webhook secret from Stripe

--- checkout_server/settings.py
+++ checkout_server/settings.py
@@ -15,7 +15,7 @@ PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY',
 SECRET_KEY = os.getenv('env.STRIPE_SECRET_KEY',
                        'sk_test_qa9ceFzzUpWu3EvMHsoHs65d')
 WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET',
-                           'whsec_HSBxvRxy7CSabcohOhxmANBA79OhqB2R')
+                           'whsec_TUjpJ0NduzHQfHFciBPN5JcsmECA97Bj')
 
 # Checkout form
 CHECKOUT_COUNTRY = 'DE'
